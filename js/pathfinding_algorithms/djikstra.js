ac90b648bd2d8639e1521ab5fd81ff349f793c9e
AdarshNaidu <naidu.adarsh2511@gmail.com>
2020-02-27 19:43:35 +0530

    djikstra's algorithm grid

--- js/pathfinding_algorithms/djikstra.js
+++ js/pathfinding_algorithms/djikstra.js
@@ -2,13 +2,19 @@ let dijkstra = {
     run: async function () {
         let t1 = performance.now();
         pathfind.begin();
+        let dist = Array(grid.width * grid.height);
         let cameFrom = {};
         let q = new MinHeap();
         q.push(grid.startPos, 0);
+        dist[grid.startPos.toInt()] = 0;
         while (!q.empty()) {
             let cur = q.pop();
             await pathfind.sleep(10);
-            
+            grid.setColor(cur, 'explored');
+
+            if (cur.equals(grid.goalPos)) {
+                break;
+            }
 
             let n = pathfind.neighborsDiag(cur);
             for (let i = 0; i < n.length; i++) {
@@ -16,11 +22,17 @@ let dijkstra = {
                 if (dist[intPos] != null) {
                     continue;
                 }
-               
+                grid.setColor(n[i].pos, 'visited');
+                dist[intPos] = n[i].dist + dist[cur.toInt()];
+                cameFrom[intPos] = cur.toInt();
+                q.push(n[i].pos, dist[intPos]);
             }
         }
 
-        
+        if (cameFrom[grid.goalPos.toInt()] == null) {
+            pathfind.end();
+            return;
+        }
 
         await pathfind.reconstructPath(cameFrom);
 
